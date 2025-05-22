287a30283ac6e514cc00710d4709bb129a08f7a2
oisinfrizzell26 <oisinfrizzell26@gmail.com>
2025-05-22 16:09:32 +0100

    adding the password strength checking

--- FrontEnd/RegisterPage/registerpage.cpp
+++ FrontEnd/RegisterPage/registerpage.cpp
@@ -1,29 +1,155 @@
 #include "registerpage.h"
 #include "ui_registerpage.h"
-#include <QDebug>
-#include "../LoginPage/loginpage.h"
+#include <QDebug>            //for debug output
+#include <QMessageBox>        //for displaying messages
+#include "../LoginPage/loginpage.h"   //include the login page for redirection
 
-// Constructor: Initializes the RegisterPage dialog and sets up the UI
+// Constructor
 RegisterPage::RegisterPage(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::RegisterPage)
+    QDialog(parent),  //calls the parent constructor
+    ui(new Ui::RegisterPage) //creates ui object
 {
     ui->setupUi(this);
+    ui->errorLabel->setText(""); // this clears the error message initially
+    
+    // Load common passwords
+    commonPasswords = loadCommonPasswordsFromCSV();
+    qDebug() << "Loaded" << commonPasswords.size() << "common passwords"; //should be about 10000
 }
 
-// Destructor: Deletes the UI object to free resources
+// Destructor
 RegisterPage::~RegisterPage()
 {
     delete ui;
 }
 
+//:: pretty much lets the compiler now tha loadCommonPasswordsFromCsv() is  function in RegisterPage class
+QStringList RegisterPage::loadCommonPasswordsFromCSV(){ 
+    QStringList paswords;       //list to store passwords
+    QFile file("common_passwords.csv");         //opens csv file
+    
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&file);     //create stream to read the file
+
+        //skip the header line
+        if (!in.atEnd()){
+            in.readLine();
+        }
+        
+        // Read passwords from the first column
+        while (!in.atEnd()) {
+            QString line = in.readLine();
+            QStringList fields = line.split(',');
+            if (!fields.isEmpty()) {
+                paswords << fields[0].trimmed();
+            }
+        }
+
+        file.close();    
+    } else {
+        qDebug() << "failed to open the common passwords file";
+    }
+
+    return paswords;
+}
+
+bool RegisterPage::validatePassword(const QString& password, const QString& confirmPassword, QString& errorMsg){
+    
+    //First check if the passwords match
+    if (password!= confirmPassword){
+        errorMsg= "Passwords dont match";
+        return false;
+    } 
+
+    // Check the minimum length
+    if (password.length() < 8){
+        errorMsg = "Password must be at least 8 characters long";
+        return false;
+    }
+
+    if (commonPasswords.contains(password.toLower())){
+        errorMsg = "This password is too commonly used ";
+        return false;
+    }
+
+    bool hasRepeats = false;
+    for (int i = 0; i < password.length() - 2; i++){
+        if (password[i] == password[i + 1] && password[i] == password[i+2]){
+            hasRepeats = true;
+            break;
+        }
+    }
+
+    if (hasRepeats){
+        errorMsg = "password contains too many repeated characters";
+        return false;
+    }
+    
+    return true;
+}
+
+// Validates username
+bool RegisterPage::validateUsername(const QString& username, QString& errorMsg)
+{
+    if (username.isEmpty()) {
+        errorMsg = "Username cannot be empty";
+        return false;
+    }
+    
+    if (username.length() < 6) {
+        errorMsg = "Username must be at least 6 characters long";
+        return false;
+    }
+    
+    // Check for spaces
+    if (username.contains(' ')) {
+        errorMsg = "Username cannot contain spaces";
+        return false;
+    }
+    
+    return true;
+}
+
 // Slot for handling the registerButton's clicked signal
 void RegisterPage::on_registerButton_clicked()
 {
-    qDebug() << "registerButton_clicked";
-    // TODO: Implement registration functionality
-    // - Validate that both password fields match
-    // - Save new user credentials to database
+    qDebug() << "registerButton_clicked"; //remove this later
+    
+    QString username = ui->usernameLineEdit->text();
+    QString password = ui->passwordLineEdit->text();
+    QString confirmPassword = ui->confirmPasswordLineEdit->text();
+    QString errorMessage;
+    
+    // Validate username
+    if (!validateUsername(username, errorMessage)) {
+        ui->errorLabel->setText(errorMessage);
+        ui->errorLabel->setStyleSheet("color: red");
+        return;
+    }
+    
+    // Validate password
+    if (!validatePassword(password, confirmPassword, errorMessage)) {
+        ui->errorLabel->setText(errorMessage);
+        ui->errorLabel->setStyleSheet("color: red");
+        return;
+    }
+    
+    // If we get here, validation passed
+    
+    // TODO: Save new user credentials to database
+    
+    // Show success message
+    QMessageBox::information(this, "Registration Successful", 
+                          "Your account has been created successfully.\n"
+                          "You will now be redirected to the login page.");
+    
+    // Navigate to login page
+    LoginPage loginDialog(nullptr);
+    loginDialog.setAttribute(Qt::WA_DeleteOnClose);
+    
+    this->accept(); // Close RegisterPage
+    
+    loginDialog.exec(); // Show LoginPage modally
 }
 
 // Slot for handling the backToLoginButton's clicked signal
--- FrontEnd/RegisterPage/registerpage.h
+++ FrontEnd/RegisterPage/registerpage.h
@@ -1,7 +1,12 @@
 #ifndef REGISTERPAGE_H
 #define REGISTERPAGE_H
 
+
 #include <QDialog>
+#include <QStringList>
+#include <QFile>
+#include <QTextStream>
+
 
 namespace Ui {
 class RegisterPage;
@@ -10,11 +15,11 @@ class RegisterPage;
 // Defines the RegisterPage dialog window.
 class RegisterPage : public QDialog
 {
-    Q_OBJECT 
+    Q_OBJECT   //this is a Qt macro for enabling signals and slots etc
 
 public:
-    explicit RegisterPage(QWidget *parent = nullptr);
-    ~RegisterPage();
+    explicit RegisterPage(QWidget *parent = nullptr); // explicit prevents implicit type conversion i
+    ~RegisterPage(); //destructor
 
 private slots:
     void on_registerButton_clicked();
@@ -22,6 +27,19 @@ private slots:
 
 private:
     Ui::RegisterPage *ui; // Pointer to the auto-generated UI class
+
+    //Nist SP800-63B password validation
+
+    bool validatePassword(const QString& password, const QString& confirmPassword, QString& errorMsg);
+    bool validateUsername(const QString& username, QString& errorMsg); //function can change error message but cant change username
+
+
+    //methods for loading common passwords there in a csv file for now
+    QStringList loadCommonPasswordsFromCSV();
+    QStringList commonPasswords; 
+
+    int failed;
+
 };
 
 #endif 
