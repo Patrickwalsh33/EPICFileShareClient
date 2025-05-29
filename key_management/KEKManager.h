e8619733d2406071f00bed8cdd259a7b0397eb6a
Patrickwalsh33 <patrickwalsh33@outlook.com>
2025-05-29 22:44:28 +0100

    made 2 functions static

--- key_management/KEKManager.h
+++ key_management/KEKManager.h
@@ -7,8 +7,8 @@ class KEKManager {
 
     EncryptionKeyGenerator* keyGenerator;
 public:
-    std::vector<unsigned char> encryptKEK(const std::vector<unsigned char>& masterKey, const std::vector<unsigned char>& kek,std::vector<unsigned char>& nonceOut);
-    std::vector<unsigned char> decryptKEK(const std::vector<unsigned char>& masterKey, const std::vector<unsigned char>& kek, const std::vector<unsigned char>& nonce);
+    static std::vector<unsigned char> encryptKEK(const std::vector<unsigned char>& masterKey, const std::vector<unsigned char>& kek,std::vector<unsigned char>& nonceOut);
+    static std::vector<unsigned char> decryptKEK(const std::vector<unsigned char>& masterKey, const std::vector<unsigned char>& kek, const std::vector<unsigned char>& nonce);
     static void generateAndStoreUserKeys(const std::vector<unsigned char>& kek);
     static void decryptAndStoredUserKeys(const std::vector<unsigned char>& kek);
 
