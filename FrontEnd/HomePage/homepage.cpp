b6714efa31ba8121c66b817547b732ea8ce33263
oisinfrizzell26 <oisinfrizzell26@gmail.com>
2025-06-01 13:46:20 +0100

    adding redirect to uplaod file page

--- FrontEnd/HomePage/homepage.cpp
+++ FrontEnd/HomePage/homepage.cpp
@@ -1,8 +1,7 @@
 #include "homepage.h"
-#include "ui_homepage.h" // Will be generated from homepage.ui
+#include "ui_homepage.h" 
 #include "../ProfilePage/ProfilePage.h"
-// #include "../UploadPage/uploadpage.h" // Removed
-// #include "../LoginPage/loginpage.h"   // Removed
+#include "../UploadPage/uploadpage.h"
 #include <QDebug>
 
 HomePage::HomePage(const QString &username, QWidget *parent) :
@@ -25,5 +24,13 @@ void HomePage::on_profileButton_clicked()
     ProfilePage *profilePage = new ProfilePage(currentUsername, this);
     profilePage->setAttribute(Qt::WA_DeleteOnClose);
     profilePage->exec();
+}
+
+void HomePage::on_uploadButton_clicked()
+{
+    qDebug() << "Upload button clicked on HomePage for user:" << currentUsername;
+    UploadPage *uploadPage = new UploadPage(this);
+    uploadPage->setAttribute(Qt::WA_DeleteOnClose);
+    uploadPage->exec();
 }
 
